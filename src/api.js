a70a8879449558ff3360ed7e74dac37bd509d323
SvitLana1990 <svitlana112@outlook.es>
2023-11-12 19:39:30 +0100

    1

--- src/api.js
+++ src/api.js
@@ -1,4 +1,30 @@
 import axios from "axios";
 
 const API_KEY = '39758797-2603f3af911ae2369cae9d72d';
-const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export const fetchImages = async (valueSearch) => {
+const response = await axios.get(`?key=${API_KEY}&q=${valueSearch}`);
+    return response.data;
+}
+
+// export async function fetchImages(valueForm,page) {
+//     try {
+//         const response = await axios.get('https://pixabay.com/api/', {
+//             params: {
+//                 key: API_KEY,
+//                 q: valueForm,
+//                 image_type: 'photo',
+//                 orientation: 'horizontal',
+//                 safesearch: true,
+//                 per_page: 12,
+//                 page: page,
+//             },
+//         });
+//         return await response.data;
+//     } catch (error) {
+//         return Notiflix.Report.failure(
+//         'Oops! Something went wrong! Try reloading the page!',
+//       );
+//     }
+// }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import { GlobalStyle } from "GlobalStyle";
 import { PictureSearchBar } from "./Searchbar/Searchbar";
 import { GalleryList } from "./ImageGallery/ImageGallery.styled";
@@ -6,14 +7,47 @@ import { ImageLoader } from "./Loader/Loader";
 import { Container } from "./App.styled";
 
 
-export const App = () => {
-  return (
-    <Container>
-      <PictureSearchBar />
-      <ImageLoader/>
-      <GalleryList />
-      <LoadMoreButton/>
-    <GlobalStyle />
-  </Container>
-  );
+export class App extends Component {
+  state = {
+    images: [],
+    valueSearch: '',
+    page: 1,
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.valueSearch !== this.state.valueSearch || prevState.page !== this.state.page) {
+      // ОТРЕЗАТЬ ID ЗАПРОСА ИЗ QUERY
+      // делаем http запрос с query и page
+      // записываем результат в images
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      valueSearch: `${Date.now()}/${event.target.value}`,
+      page: 1,
+      images: []
+    });
+  }
+
+  handleLoadMore = () => {
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <PictureSearchBar onSubmit={this.handleSubmit} />
+        <ImageLoader />
+        <GalleryList />
+        <LoadMoreButton onClick={this.handleLoadMore} />
+        <GlobalStyle />
+      </Container>
+    )
+  };
 };
